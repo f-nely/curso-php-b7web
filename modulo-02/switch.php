3da84ef149a5be1542740bd69e4cf8fbbaf58713
nely de faria <nelly.nf54@gmail.com>
2021-12-13 12:33:29 -0300

    Criando arquivo switch.php

---
+++ modulo-02/switch.php
@@ -0,0 +1,15 @@
+<?php
+
+$type = 'photo';
+
+switch ($type) {
+  case 'photo':
+    echo 'Show a photo';
+    break;
+  case 'glass':
+    echo 'Show a glass';
+    break;
+  case 'text':
+    echo 'Show a text';
+    break;
+}
