6e5be368668bef600640c7abfe67388cd4e7bda2
nely de faria <nelly.nf54@gmail.com>
2021-12-14 11:47:45 -0300

    Criando arquivo definindo_funcao.php

---
+++ modulo-03/definindo_funcao.php
@@ -0,0 +1,12 @@
+<?php
+
+function familyName(string $fname)
+{
+  echo "$fname Refsnes. <br>";
+}
+
+familyName('Jani');
+familyName('Hege');
+familyName('Stale');
+familyName('Kay Jim');
+familyName('Borge');
