f04960a355ca31968bde5c489bf8801149dc7b12
nely de faria <nelly.nf54@gmail.com>
2021-12-16 16:15:20 -0300

    Criando arquivo funcoes_matematicas.php

---
+++ modulo-03/funcoes_matematicas.php
@@ -0,0 +1,26 @@
+<?php
+
+$n = -8.4;
+$n2 = 2.7;
+$n3 = 4.1;
+$n4 = 12.498173;
+$aleatorio = rand(3, 9);
+$list = [4, 1, 9, 8];
+
+echo abs($n) . '<br>';
+
+echo pi() . '<br>';
+
+echo floor($n2) . '<br>';
+
+echo ceil($n2) . '<br>';
+
+echo round($n3) . '<br>';
+
+echo round($n4, 2) . '<br>';
+
+echo $aleatorio . '<br>';
+
+echo max($list) . '<br>';
+
+echo min($list) . '<br>';
